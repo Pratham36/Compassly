94e70e8bc6a448bf8b1ad42f697893e79d4825a3
Hardik-Ashra <hardikashra1234@gmail.com>
2025-08-05 18:05:24 +0530

    resume add

---
+++ app/(main)/resume/_components/templates.js
@@ -0,0 +1,17 @@
+export const resumeTemplates = {
+  classic: {
+    name: "Classic",
+    markdownStyles: "classic", // Use this to conditionally render style
+    description: "A timeless, professional layout.",
+  },
+  modern: {
+    name: "Modern",
+    markdownStyles: "modern",
+    description: "Bold headings with a modern layout.",
+  },
+  elegant: {
+    name: "Elegant",
+    markdownStyles: "elegant",
+    description: "Sleek and minimal design with good spacing.",
+  },
+};
